12d096146398a14c19333edb2de2d20fd2b5dbc7
Chen <chenziwe@fasenmyer.cs.grinnell.edu>
2018-12-22 23:05:48 -0600

    added grinnell predict

--- process_grinnell.py
+++ process_grinnell.py
@@ -4,8 +4,11 @@ import subprocess
 import os
 import sys
 
+import res50 as r50
+
 grinnell_path="grinnell_faces/unprocessed/"
 grinnell_processed_path="grinnell_faces/processed/"
+grinnell_score_file="grinnell_faces/score.txt"
 
 def process_all():
     cmd = "ls "+grinnell_path
@@ -18,4 +21,37 @@ def process_all():
         img = cv2.resize(img,(224,224))
         cv2.imwrite(grinnell_processed_path+f,img)
 
-process_all()
+def record_all_score():
+    cmd = "ls "+grinnell_processed_path
+    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    filenames=output.split()
+    with file(grinnell_score_file,'w') as outfile:
+        for f in filenames:
+            img = cv2.imread(grinnell_processed_path+f,cv2.IMREAD_UNCHANGED)
+            img = img.transpose((2, 0, 1))
+            score = r50.get_score(img)
+            np.savetxt(outfile, score)
+
+def predict(img):
+    img = cv2.resize(img,(224,224))
+    img = img.transpose((2, 0, 1))
+    score = r50.get_score(img)
+    cmd = "ls "+grinnell_path
+    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    filenames=output.split()
+    cur_predict=""
+    cur_dis_min=sys.maxint
+    scores = np.loadtxt(grinnell_score_file).astype(np.float)
+    for i in range(0,len(filenames)):
+        s=scores[i]
+        dis=(s-score).pow(2).sum()
+        if dis<cur_dis_min:
+            cur_dis_min=dis
+            cur_predict=filenames[i][:-4]
+    print "Prediction is "+cur_predict+"!!!"
+    return cur_predict
+
+#process_all()
+#record_all_score()
